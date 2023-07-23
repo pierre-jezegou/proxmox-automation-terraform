35947f57babdbcf029bccd352d0e88252c67b946
Pierre Jézégou <pierre.jezegou@centrale.centralelille.fr>
2023-07-23 16:27:47 +0200

    First commit

---
+++ lxc-demo.tf
@@ -0,0 +1,40 @@
+resource "proxmox_lxc" "lxc-demo" {
+    target_node     = var.TARGET_NODE
+    hostname        = "lxc-basic"
+    ostemplate      = "${var.EXTERNAL_STORAGE}:template/cache/debian-12-standard_12.0-1_amd64.tar.zst"
+    password        = var.LCX_PASSWORD
+    unprivileged    = false
+    start           = true
+    vmid            = 0 # Next available vmid used
+    cores           = 1
+    description     = "LXC Terraform template"
+
+    rootfs {
+        storage = "nas"
+        size    = "3G"
+    }
+
+    network {
+        name    = "eth0"
+        bridge  = "vmbr0"
+        ip      = "dhcp"     
+    }
+
+    # No nameserver specified -> Proxmox host values
+}
+
+
+#### VARIABLES ####
+
+variable "LCX_PASSWORD" {
+    type = string
+    sensitive = true
+}
+
+variable "TARGET_NODE" {
+    type = string
+}
+
+variable "EXTERNAL_STORAGE" {
+    type = string
+}
